ef6933ea5f1f7644b71082bd260ecc12cd8d5d69
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2023-02-10 22:42:29 +0100

    Update firstRepeatingChar.java file

--- workspace/java/firstRepeatingChar.java
+++ workspace/java/firstRepeatingChar.java
@@ -4,6 +4,7 @@ public class firstRepeatingChar {
   public static void main(String[] args) {
     String string = "responsible";
     System.out.println(findFirstRepeatingCharacter(string));
+    System.out.println(findFirstRepeatingCharacter2(string));
   }
 
   static char findFirstRepeatingCharacter(String string) {
@@ -13,6 +14,16 @@ public class firstRepeatingChar {
           return string.charAt(outerIndex);
         }
       } 
+    }
+    return '\0';
+  }
+
+  static char findFirstRepeatingCharacter2(String string){
+    HashMap<Character,Boolean> visited = new HashMap<Character, Boolean>();
+    char[] characters = string.toCharArray(); // Converts the string to a character array
+    for(char character : characters){
+      if(visited.containsKey(character)) return character;
+      else visited.put(character, true);
     }
     return '\0';
   }
