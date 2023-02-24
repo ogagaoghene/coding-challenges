44d1e273d5f03b9347fedc68dabe51adf3ef23df
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2023-02-24 13:20:47 +0100

    Add Swap.java

--- workspace/java/Swap.java
+++ workspace/java/Swap.java
@@ -3,9 +3,21 @@ public class Swap {
     int temp = left;
     left = right;
     right = temp;
-    System.out.printf("%d, %d", left, right);
+    System.out.printf("%d, %d\n", left, right);
   }
+
+  static void swapArray(int[] list) {
+    if(list != null && list.length == 2) {
+      int temp = list[0];
+      list[0] = list[1];
+      list[1] = temp;
+    }
+  }
+
   public static void main(String[] arg) {
     swap(2,3);
+    int[] sample = {10,20};
+    swapArray(sample);
+    System.out.printf("%d %d\n", sample[0], sample[1]);
   }
 }
