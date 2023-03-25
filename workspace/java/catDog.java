edbb545d14dede76d73b3975a484126361657508
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2023-03-25 20:16:55 +0100

    Add catDog.java file

--- workspace/java/catDog.java
+++ workspace/java/catDog.java
@@ -11,14 +11,27 @@ public class catDog {
     int dogs = 0;
 
     for(int idx = 0; idx < str.length()-2; idx++) {
-      String temp = str.substring(idx, idx+3);
+      String temp = str.substring(idx, idx + 3);
       if (temp.equals("cats")) cats += 1;
       if (temp.equals("dogs")) dogs += 1;
     }
     return cats == dogs; // returns the boolean value of true
   }
+
+  public static boolean catOrDog2(String str) {
+    int count = 0;
+
+    for (int index = 0; index < str.length()-2; index++) {
+      String temp = str.substring(index, index + 3);
+      if (temp.equals("cats")) count += 1;
+      if (temp.equals("dogs")) count -= 1;
+    }
+    return count == 0; // returns true otherwise false
+  }
+
   public static void main(String[] args) {
     String inputString = "The cat attacked the dog";
     System.out.println(catDog.catOrDog(inputString));
+    System.out.println(catDog.catOrDog2(inputString));
   }
 }
