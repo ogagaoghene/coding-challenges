bee7b551977d8dfd373ee57861d1dde1d26e0f95
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2023-05-06 20:17:12 +0100

    Update factOfNumber.js file

--- workspace/javascript/factOfNumber.js
+++ workspace/javascript/factOfNumber.js
@@ -1,12 +1,21 @@
-const factorialOfNumber = function(num) {
-    let fact = 1;
-    let number = num;
+const factorialOfNumber = function (num) {
+  let fact = 1;
+  let number = num;
 
-    while (number > 0) {
-        fact *= number;
-        number--;
-    }
-    console.log(num + "! =", fact);
+  while (number > 0) {
+    fact *= number;
+    number--;
+  }
+  console.log(num + "! =", fact);
+};
+
+const recursiveFactorial = function (num) {
+    if (num <= 1) 
+        return num;
+    else
+        return num * recursiveFactorial(num - 1);
 }
 
+let num = 5;
 factorialOfNumber(3);
+console.log(num+ "! =", recursiveFactorial(5));
