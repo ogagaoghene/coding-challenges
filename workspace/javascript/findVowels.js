19aeec98cd33b655f2b5464e6ea7d247e03c044f
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2023-07-15 23:25:03 +0100

    Modify findVowels.js file

--- workspace/javascript/findVowels.js
+++ workspace/javascript/findVowels.js
@@ -10,5 +10,18 @@ function findVowels(str) {
     return count;
 }
 
+
+const Vowels = (str) => {
+    let count = 0;
+    const checker = ['a','e','i','o','u'];
+
+    for (let char of str.toLowerCase()) {
+        if (checker.includes(char))
+            count++;
+    }
+    return count;
+}
+
 const sample_string = "Hello";
 console.log(findVowels(sample_string));
+console.log(Vowels(sample_string));
