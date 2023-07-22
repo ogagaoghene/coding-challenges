d6a3b259d7e1341038e44fe461c5c657182f4b28
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2023-07-22 20:36:47 +0100

    Add Main.java & StringReverser.java files

---
+++ workspace/java/Stack/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] argv) {
+        String input = "Banana";
+        StringReverser reverser = new StringReverser();
+        String output = reverser.reverse(input);
+        System.out.println(output);
+    }
+}
---
+++ workspace/java/Stack/StringReverser.java
@@ -0,0 +1,17 @@
+import java.util.Stack;
+
+public class StringReverser {
+    public String reverse(String input) {
+        Stack<Character> stack = new Stack<>();
+
+        for(char ch : input.toCharArray()) {
+            stack.push(ch);
+        }
+        StringBuffer reversed = new StringBuffer();
+        while(!stack.empty()) {
+            reversed.append(stack.pop());
+        }
+
+        return reversed.toString();
+    }
+}
