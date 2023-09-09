660caf3926681a302fb98b22fdea9abeb1832f7e
Esi Ogagaoghene <30408871+ogagaoghene@users.noreply.github.com>
2023-09-09 21:31:58 +0100

    Delete workspace directory

--- workspace/javascript/convert-json.js
+++
@@ -1,8 +0,0 @@
-const book = {
-    title: "God Father",
-    author: "Mario Puzo"
-}
-
-const bookJSON = JSON.stringify(book);
-const bookObject = JSON.parse(bookJSON);
-console.log(bookObject.title);
--- workspace/javascript/isPalindrome.js
+++
@@ -1,9 +0,0 @@
-let isPalindrome = (function(str) {
-    const reversed = str.split('').reverse().join('');
-    if (str === reversed) {
-        return true;
-    }
-    return false;
-})("mute");
-
-console.log(isPalindrome);
