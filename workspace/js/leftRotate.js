da6428145b36bb72a1544c5fd9d3a660e5047937
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2023-09-29 23:00:41 +0100

    Add leftRotate.js file for left rotating an array K times

---
+++ workspace/js/leftRotate.js
@@ -0,0 +1,22 @@
+const leftRotate = (array, k) => {
+    let temp = 0;
+    const len = array.length;
+    k = k % len;
+
+    for (let idx = 0; idx < k; idx++) {
+        temp = array.shift();
+        array.push(temp);
+    }
+    return array;
+}
+
+let arr = [1,2,3,4];
+let shifts = 2;
+
+console.log(leftRotate(arr, 2));
+
+[1,2,3,4]
+k = 1
+2,3,4,1
+k = 2
+3,4, 1, 2
